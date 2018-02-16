9d73e2f004c1a9bff1ffa2e1fb597bf18a0c2b02
Courtney Fricano <courtney.fricano@analog.com>
2018-02-16 10:56:29 -0500

    Add irun message types to log4j mode regex matching

--- setup-files/setup-log.el
+++ setup-files/setup-log.el
@@ -1,9 +1,13 @@
-;; Time-stamp: <2017-10-20 08:04:15 cfricano>
+;; Time-stamp: <2018-02-16 10:55:41 cfricano>
 
 ;; log4j-mode
 (autoload 'log4j-mode "log4j-mode" "Major mode for viewing log files." t)
 (add-to-list 'auto-mode-alist '("\\.log\\'" . log4j-mode))
 
+(setq log4j-match-fatal-regexp "\\<\\(FATAL\\|\\\\*F\\)\\>")
+(setq log4j-match-error-regexp "\\<\\(ERROR\\|SEVERE\\|\\\\*E\\)\\>")
+(setq log4j-match-warn-regexp "\\<\\(WARN\\(?:ING\\)?\\|\\\\*W\\)\\>")
+(setq log4j-match-info-regexp "\\<\\(INFO\\|\\\\*I\\)\\>")
 
 (setq setup-log-loaded t)
 (provide 'setup-log)
