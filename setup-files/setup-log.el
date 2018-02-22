861354333d2bff61a596a1bb44251013b4fc6392
Courtney Fricano <courtney.fricano@analog.com>
2018-02-22 09:30:22 -0500

    Extra log4j message tags

--- setup-files/setup-log.el
+++ setup-files/setup-log.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-02-16 10:55:41 cfricano>
+;; Time-stamp: <2018-02-22 09:28:50 cfricano>
 
 ;; log4j-mode
 (autoload 'log4j-mode "log4j-mode" "Major mode for viewing log files." t)
@@ -7,7 +7,8 @@
 (setq log4j-match-fatal-regexp "\\<\\(FATAL\\|\\\\*F\\)\\>")
 (setq log4j-match-error-regexp "\\<\\(ERROR\\|SEVERE\\|\\\\*E\\)\\>")
 (setq log4j-match-warn-regexp "\\<\\(WARN\\(?:ING\\)?\\|\\\\*W\\)\\>")
-(setq log4j-match-info-regexp "\\<\\(INFO\\|\\\\*I\\)\\>")
-
+(setq log4j-match-info-regexp "\\<\\(INFO\\|\\\\*I\\|\\\\*N\\)\\>")
+(setq log4j-match-debug-regexp "\\<\\(DEBUG\\|FINE\\(?:R\\|ST\\)?\\|STATUS\\|asdf\\)\\>")
+      
 (setq setup-log-loaded t)
 (provide 'setup-log)
